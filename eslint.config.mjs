da95b129f82cd373d6d18a4423770e67ddb1e75a
Jaiden <jmt268@byu.edu>
2025-09-23 16:58:39 -0600

    Update lint config without pluginJs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,5 +6,4 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.jest } },
-  pluginJs.configs.recommended,
 ]);
