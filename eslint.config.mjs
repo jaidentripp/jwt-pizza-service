2d585812b08bc126bff4fb55fef4dcb1c54ee4ff
Jaiden <jmt268@byu.edu>
2025-09-23 16:56:24 -0600

    Update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,5 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  {
-    files: ["**/*.test.js"],
-    languageOptions: {
-      globals: {
-        ...globals.jest
-      }
-    }
-  }
+  { languageOptions: { globals: globals.jest } },
 ]);
